8bd357804b5d5393d7102c781d2a580cbb871bc7
Karl Haines <kmhnashville@gmail.com>
2024-06-23 07:59:11 -0500

    Stuff was edited and shit.

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,8 +1,8 @@
 if vim.g.neovide then
 	-- set font
 	vim.g.neovide_scale_factor = 1.0
-	vim.cmd([[set gfn=Agave\ Nerd\ Font:h8]])
-	vim.cmd([[set gfw=Noto\ Color\ Emoji:h8]])
+	vim.cmd([[set gfn=CaskaydiaCove\ Nerd\ Font:h10]])
+	vim.cmd([[set gfw=Noto\ Color\ Emoji:h10]])
 	-- setup some transparency
 	vim.g.neovide_transparency = 0.9
 	vim.g.neovide_floating_blur_amount_x = 6.0
@@ -17,11 +17,11 @@ if vim.g.neovide then
 	vim.g.neovide_cursor_vfx_particle_density = 18.0
 	vim.g.neovide_cursor_vfx_particle_speed = 12.0
 	-- setup some expected functionalities
-	vim.keymap.set("n", "<D-s>", ":w<CR>")     -- Save
-	vim.keymap.set("v", "<D-c>", '"+y')        -- Copy
-	vim.keymap.set("n", "<D-v>", '"+P')        -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P')        -- Paste visual mode
-	vim.keymap.set("c", "<D-v>", "<C-R>+")     -- Paste command mode
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
 	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 	-- Allow clipboard copy paste in neovim
 	vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
@@ -29,7 +29,7 @@ if vim.g.neovide then
 	vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 	vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 	-- setup font up/downsizing wityh ctrl- and ctrl+
-	vim.g.neovide_scale_factor = 1.0
+	-- vim.g.neovide_scale_factor = 1.0
 	local change_scale_factor = function(delta)
 		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 	end
@@ -39,4 +39,6 @@ if vim.g.neovide then
 	vim.keymap.set("n", "<C-->", function()
 		change_scale_factor(1 / 1.10)
 	end)
+elseif vim.fn.has("guirunning") < 0 then
+	vim.cmd([[set gfn=Agave\ Nerd\ Font:13]])
 end
--- lua/polish.lua
+++ lua/polish.lua
@@ -21,10 +21,5 @@ local o = vim.opt
 o.cmdheight = 1
 o.relativenumber = false
 o.autochdir = true
-
--- setup options for neovide
-if vim.g.neovide then
-  require("neovide")
-else
-  vim.cmd([[set guifont=Agave\ Nerd\ Font:h10]])
-end
+o.pumblend = 30
+o.winblend = 30
