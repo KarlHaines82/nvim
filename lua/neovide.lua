0609b48be4f31c8f6251792d84e101353f0f357d
Karl Haines <kmhnashville@gmail.com>
2025-09-04 15:22:57 -0500

    Added guifont to neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -2,7 +2,9 @@ if vim.g.neovide then
   vim.g.neovide_fullscreen = false
   vim.cmd [[set gfn=Monaco\ Nerd\ Font:h11]]
   vim.g.neovide_scale_factor = 1.0
+  -- create functions for ctrl+- and ctrl++ to adjust fontsize
   local change_scale_factor = function(delta) vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta end
   vim.keymap.set("n", "<C-=>", function() change_scale_factor(1.25) end)
   vim.keymap.set("n", "<C-->", function() change_scale_factor(1 / 1.25) end)
+  vim.g.neovide_opacity = 0.9
 end
